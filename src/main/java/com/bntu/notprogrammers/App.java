9f05a22b6678d7cdd9eeaa93803a9e2d2f220818
Alexander Kiselev <kiselev20060@gmail.com>
2017-09-07 17:25:25 +0300

    added test functions throw netbeans

--- src/main/java/com/bntu/notprogrammers/App.java
+++ src/main/java/com/bntu/notprogrammers/App.java
@@ -7,5 +7,17 @@ package com.bntu.notprogrammers;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        myFunc();
+    }
+    
+    /*
+    *
+    * @author Alexander Kiselev
+    *
+    * @see hello message
+    *
+    */
+    public static void myFunc() {
+        System.out.println("My out");
     }
 }
